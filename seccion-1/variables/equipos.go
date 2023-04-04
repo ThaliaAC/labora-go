64ff42d06503b8852e208aa96836e30739c99991
Thalia Arrascue <thalia.arrascue@gmail.com>
2023-04-04 05:19:29 -0500

    Create equipos.go

---
+++ seccion-1/variables/equipos.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+func main() {
+	var equipo1 = "Real Madrid"
+	var titulos1 = 21
+	fmt.Printf("El equipo de fútbol del %s ha ganado %d títulos.\n", equipo1, titulos1)
+	var equipo2 = "Barcelona"
+	var titulos2 = 14
+	fmt.Printf("El equipo de fútbol del %s ha ganado %d títulos.\n", equipo2, titulos2)
+	var equipo3 = "Milan"
+	var titulos3 = 14
+	fmt.Printf("El equipo de fútbol del %s ha ganado %d títulos.\n", equipo3, titulos3)
+	var equipo4 = "Liverpool"
+	var titulos4 = 13
+	fmt.Printf("El equipo de fútbol del %s ha ganado %d títulos.\n", equipo4, titulos4)
+	var equipo5 = "Bayern"
+	var titulos5 = 9
+	fmt.Printf("El equipo de fútbol del %s ha ganado %d títulos.\n", equipo5, titulos5)
+}
