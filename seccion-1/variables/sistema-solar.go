3adb2012e54b618edf296bd43f67fac1b7675193
Thalia Arrascue <thalia.arrascue@gmail.com>
2023-04-04 04:44:05 -0500

    Update sistema-solar.go

--- seccion-1/variables/sistema-solar.go
+++ seccion-1/variables/sistema-solar.go
@@ -1,4 +1,34 @@
 package main
-func main() {
+import "fmt"
 
+func main(){
+	var planetName string
+	var numberOfMoons int
+	planetName = "Mercurio"
+	numberOfMoons = 0
+	fmt.Printf("%s tiene %d lunas.\n", planetName, numberOfMoons)
+	planetName = "Venus"
+	numberOfMoons = 0
+	fmt.Printf("%s tiene %d lunas.\n", planetName, numberOfMoons)
+	planetName = "Tierra"
+	numberOfMoons = 1
+	fmt.Printf("%s tiene %d lunas.\n", planetName, numberOfMoons)
+	planetName = "Marte"
+	numberOfMoons = 2
+	fmt.Printf("%s tiene %d lunas.\n", planetName, numberOfMoons)
+	planetName = "Júpiter"
+	numberOfMoons = 79
+	fmt.Printf("%s tiene %d lunas.\n", planetName, numberOfMoons)
+	planetName = "Saturno"
+	numberOfMoons = 82
+	fmt.Printf("%s tiene %d lunas.\n", planetName, numberOfMoons)
+	planetName = "Urano"
+	numberOfMoons = 27
+	fmt.Printf("%s tiene %d lunas.\n", planetName, numberOfMoons)
+	planetName = "Neptuno"
+	numberOfMoons = 14
+	fmt.Printf("%s tiene %d lunas.\n", planetName, numberOfMoons)
+	planetName = "Plutón"
+	numberOfMoons = 5
+	fmt.Printf("%s tiene %d lunas.\n", planetName, numberOfMoons)
 }
