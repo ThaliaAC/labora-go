0445dced1cd342818e06559ce7d561555a7bef6e
Thalia Arrascue <thalia.arrascue@gmail.com>
2023-04-04 05:07:21 -0500

    Update restaurantes.go

--- seccion-1/variables/restaurantes.go
+++ seccion-1/variables/restaurantes.go
@@ -2,12 +2,12 @@ package main
 import "fmt"
 func main() {
 	restaurante1 := "Central"
-	calificacion1 := "4.6"
-	fmt.Printf("El restaurante %s tiene una calificación de %s.\n", restaurante1, calificacion1)
+	calificacion1 := 4.6
+	fmt.Printf("El restaurante %s tiene una calificación de %g.\n", restaurante1, calificacion1)
 	restaurante2 := "Maido"
-	calificacion2 := "4.7"
-	fmt.Printf("El restaurante %s tiene una calificación de %s.\n", restaurante2, calificacion2)
+	calificacion2 := 4.7
+	fmt.Printf("El restaurante %s tiene una calificación de %g.\n", restaurante2, calificacion2)
 	restaurante3 := "Mayta"
-	calificacion3 := "4.6"
-	fmt.Printf("El restaurante %s tiene una calificación de %s.\n", restaurante3, calificacion3)
+	calificacion3 := 4.6
+	fmt.Printf("El restaurante %s tiene una calificación de %g.\n", restaurante3, calificacion3)
 }
