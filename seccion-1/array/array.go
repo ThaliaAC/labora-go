b08c422b30fc8ec1e65b895ea7fc18f06eb27a85
Thalia Arrascue <thalia.arrascue@gmail.com>
2023-04-06 12:18:01 -0500

    Create array.go

---
+++ seccion-1/array/array.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(numbers)
+	names := [] string {"Alejandra", "Raisa", "Tita", "Ludmila", "Maria", "Sabrina", "Valentina","Jeraldyn", "Aracely", "Paula", "Thalia"}
+	fmt.Println(names)
+}
